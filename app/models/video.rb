dabc3d69b55521f11cea930f60eca6b689d609b7
daniagudelo <daniagudelo@gmail.com>
2016-12-15 20:03:49 +0000

    Commit to see which repository this is

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,2 +1,12 @@
 class Video < ActiveRecord::Base
+    #This validates the presence of title
+    validates :title, presence: true, length: {maximum: 140}
+    #This validates the presence of preview link
+    validates :link, presence: true
+    #This validates the presence of actress
+    validates :starring, presence: true
+    #This validates the presence of a description
+    validates :description, presence: true
+    
+    belongs_to :user
 end
